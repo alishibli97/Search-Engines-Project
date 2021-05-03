925edd7799f1f65bd5544f7059837e6c531d5597
Carl Johan Widman <Cojen@Mikaels-MacBook-Air.local>
2021-05-03 22:22:50 +0200

    added some logging, csv-to-json, read of the entire dataset and a basic result computation

--- src/reader.py
+++ src/reader.py
@@ -1,21 +1,87 @@
 # Import Elasticsearch package 
 from elasticsearch import Elasticsearch 
 import json
+import csv
+import logging
+from time import process_time
+
 # Connect to the elastic cluster
 es=Elasticsearch([{'host':'localhost','port':9200}])
 
-ID = 0
+ID = -1
 def generateID():
     global ID
     ID = ID + 1
     return ID
 
-es.indices.delete(index='books', ignore=[400, 404])          #delete index
+def make_json(csvFilePath, jsonFilePath):
+     
+    # create a dictionary
+    data = {}
+     
+    # Open a csv reader called DictReader
+    with open(csvFilePath, encoding='utf-8') as csvf:
+        csvReader = csv.DictReader(csvf)
+         
+        # Convert each row into a dictionary
+        # and add it to data
+        for rows in csvReader:
+             
+            # Assuming a column named 'No' to
+            # be the primary key
+            key = generateID()
+            data[key] = rows
+ 
+    # Open a json writer, and use the json.dumps()
+    # function to dump data
+    with open(jsonFilePath, 'w', encoding='utf-8') as jsonf:
+        jsonf.write(json.dumps(data, indent=4))
 
-with open('books.txt', 'r') as f:
-    books = json.load(f)
-        
 
-for i in range(len(books)):
-    #store document in elasticsearch 
-    res = es.index(index='books', id=generateID(), body=books[i])
+def add_to_index(books):
+    for i in range(len(books)):
+        if(i%1000==0):
+            logger.info(f'{i} out of {len(books)} indexed')
+        #store document in elasticsearch 
+        res = es.index(index='books', id=generateID(), body=books[str(i)])
+
+
+def main():
+    # Driver Code
+    
+    # Decide the two file paths according to your
+    # computer system
+    csv_file_path = './src/goodreads_books.csv'
+    json_file_path = './src/goodreads_books.json'
+    
+    # Call the make_json function
+    # make_json(csv_file_path, json_file_path)
+
+    logger.info('Deleting index')
+    es.indices.delete(index='books', ignore=[400, 404])          #delete index
+
+    logger.info('Loading JSON')
+
+    with open(json_file_path, 'r') as f:
+
+        books = json.load(f)
+    start = process_time()
+    add_to_index(books)
+    logger.info(f'Indexing took {process_time() - start} seconds')
+
+
+if __name__ == "__main__":
+    # logging setup
+    es_logger = logging.getLogger('elasticsearch')
+    es_logger.propagate = False
+    es_logger.setLevel(logging.INFO)
+    logger = logging.getLogger('mainLog')
+    logger.propagate = False
+    logger.setLevel(logging.INFO)
+    consoleHandler = logging.StreamHandler()
+    consoleHandler.setLevel(logging.INFO)
+    formatter = logging.Formatter('%(asctime)s:%(name)s:%(levelname)s:%(message)s')
+    consoleHandler.setFormatter(formatter)
+    logger.addHandler(consoleHandler)
+    
+    main()
--- src/searcher.py
+++ src/searcher.py
@@ -3,37 +3,75 @@
 from elasticsearch import Elasticsearch 
 import json
 import argparse
-
-es=Elasticsearch([{'host':'localhost','port':9200}])      # Connect to the elastic cluster
-"""
-Basic script to create an empty python package containing one module
-"""
+import logging
+from time import process_time
+from collections import defaultdict
+from math import sqrt
 
 
 class Searcher():
-    
-    def search(self, tag, value):
-        result = self.es.search(index='books', body={
-        'query':{
-            'match':{
-                tag:value
-            }
-        }})
-        
-        for hit in result['hits']['hits']:
-            print(hit['_source']['title'])
-            
+
     def __init__(self):
         self.es=Elasticsearch([{'host':'localhost','port':9200}])
+        self.search_weights = {
+            'tags' : 0.25
+            , 'name' : 0.25
+            , 'author' : 0.25
+            , 'abstract' : 0.25
+        }
+
+    def search(self, query):
+        result = defaultdict(int)
+
+        for metric, weight in self.search_weights.items():
+            temp_result = self.es.search(index='books', size='10', body={
+                'query':{
+                    'match':{
+                        metric:query
+                    }
+                }
+            })
+            logger.info(f'Hits for "{query}" in "{metric}"')
+            for hit in temp_result['hits']['hits']:
+                # logging
+                metric_value = hit['_source'][metric][:100]+'...' if len(hit['_source'][metric])>100 else hit['_source'][metric]
+                logger.info('{0:<10} {1:<105} {2:>8}'.format(hit['_source']['book_id'],  metric_value, hit['_score']))
+                
+                # score calculated as: es_score * given_weight * average_rating * sqrt(number_of_ratings)
+                result[hit['_source']['name']] += hit['_score'] * weight * float(hit['_source']['average_rating']) * sqrt(int(hit['_source']['number_of_ratings']))
+
+        return dict(sorted(result.items(), key=lambda item: item[1], reverse=True)) # re-sort dict on calulated score
 
 
-def main():
-    parser = argparse.ArgumentParser(description='Searcher')
-    parser.add_argument('--tag', '-t', type=str, help='tag to search by')
-    parser.add_argument('--value', '-v', type=str, help='value to search by')
-    args = parser.parse_args()
+def main(args):
     searcher = Searcher()
-    searcher.search(args.tag, args.value)
+    start = process_time()
+    res = searcher.search(args.query)
+
+    logger.info(f'Query took {(process_time() - start):.3f} seconds to complete')
+    logger.info('{:^70}'.format('FINAL RESULTS'))
+    logger.info('{0:<60} {1:>8}'.format('BOOK_TITLE', 'TOTAL_SCORE'))
+    logger.info('{0:<60} {1:>8}'.format('----------', '-----------'))
+    for k,v in res.items():
+        logger.info('{0:<60} {1:>8.3f}'.format(k,v))
+
 
 if __name__ == '__main__':
-    main()
+    # logging setup
+    logger = logging.getLogger('mainLog')
+    logger.propagate = False
+    logger.setLevel(logging.INFO)
+    consoleHandler = logging.StreamHandler()
+    consoleHandler.setLevel(logging.INFO)
+    formatter = logging.Formatter('%(asctime)s:%(name)s:%(levelname)s: - %(message)s')
+    consoleHandler.setFormatter(formatter)
+    logger.addHandler(consoleHandler)
+
+    # parse arguments
+    parser = argparse.ArgumentParser(description='Searcher')
+    # parser.add_argument('--tag', '-t', type=str, help='tag to search by')
+    # parser.add_argument('--value', '-v', type=str, help='value to search by')
+    parser.add_argument('--query', '-q', type=str, help='search query')
+    args = parser.parse_args()
+
+    main(args)
